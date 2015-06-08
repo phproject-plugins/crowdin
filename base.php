b5fee537306ede817f8038bcb42348f9b91d9961
Alan Hardman <ahardman@thrivelife.com>
2015-06-08 16:23:33 -0600

    Adding base code

---
+++ base.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package  Crowdin
+ * @author   Alan Hardman <alan@phpizza.com>
+ * @version  1.0.0
+ */
+
+namespace Plugin\Crowdin;
+
+class Base extends \Plugin {
+
+	protected
+		$locale = "ach-UG",
+		$jsCode = <<<EOT
+<script type="text/javascript">
+  var _jipt = [];
+  _jipt.push(['project', 'phproject']);
+</script>
+<script type="text/javascript" src="//cdn.crowdin.com/jipt/jipt.js"></script>
+EOT;
+
+	/**
+	 * Initialize the plugin
+	 */
+	public function _load() {
+		$f3 = \Base::instance();
+		$f3->mset(array(
+			"LOCALES" => "app/plugin/crowdin/dict-crowdin/",
+			"FALLBACK" => $this->locale,
+			"LANGAUGE" => $this->locale,
+		));
+		$this->_addJs($this->jsCode);
+	}
+
+}
